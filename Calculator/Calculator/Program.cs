5b6633bbf62ed19ebb901cdfcc895e5f1b8c4e66
Pasha-Els <78309888+Pasha-Els@users.noreply.github.com>
2021-06-19 15:02:34 +0300

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            BigInteger a = new BigInteger();
+            BigInteger b = new BigInteger();
+            Console.WriteLine("Введите первое значение");
+            a = BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второое значение");
+            b = BigInteger.Parse(Console.ReadLine());
+            BigInteger sum = BigInteger.Abs(a + b);
+            BigInteger sub = BigInteger.Abs(a - b);
+            BigInteger mult = BigInteger.Abs(a * b);
+            BigInteger ost = new BigInteger();
+            BigInteger div = BigInteger.DivRem(a, b, out ost);
+            Console.WriteLine("Сумма = "+sum);
+            Console.WriteLine("Разность = "+sub);
+            Console.WriteLine("Произведение = "+mult);
+            Console.WriteLine("Частное = " + div);
+            Console.WriteLine("Остаток от деления = "+ost);
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
